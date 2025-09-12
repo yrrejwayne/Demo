a17569d78baea15f4e759f23519a270371545149
yrrej <yrrejwaynevillaflores@gmail.com>
2025-09-12 20:00:49 +0800

    Commit 1st dev

--- src/test/java/pinkcloud/ExtentReports/FourthFile.java
+++ src/test/java/pinkcloud/ExtentReports/FourthFile.java
@@ -1,5 +1,31 @@
 package pinkcloud.ExtentReports;
 
+import org.testng.annotations.Test;
+
 public class FourthFile {
+	@Test
+	public void test1() {
+		System.out.println("test");
+	}
+	
+	@Test
+	public void test2() {
+		System.out.println("test");
+	}
+	
+	@Test
+	public void test3() {
+		System.out.println("test");
+	}
+	
+	@Test
+	public void test4() {
+		System.out.println("test");
+	}
+	
+	@Test
+	public void test15() {
+		System.out.println("test");
+	}
 
 }
